00bf1cffb0b677675a851f8198f738680348783c
GMV <gmv@GMVs-MacBook-Pro.local>
2020-02-27 16:24:30 +0700

    first Commit

---
+++ app/src/main/java/latihan/kotlin/implicitintents/MainActivity.kt
@@ -0,0 +1,38 @@
+package latihan.kotlin.implicitintents
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnUrl = findViewById<Button>(R.id.open_website_button)
+        val btnLoc = findViewById<Button>(R.id.open_location_button)
+        val btnText= findViewById<Button>(R.id.share_text_button)
+
+        val URL_ADDRES = findViewById<EditText>(R.id.website_edittext)
+
+        btnUrl.setOnClickListener {
+            Toast.makeText(this,"Open Web Page",Toast.LENGTH_SHORT).show()
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://www.javatpoint.com/"))
+            startActivity(Intent.createChooser(intent,"Choose The Browser :"))
+        }
+
+        btnLoc.setOnClickListener {
+            Toast.makeText(this,"Share Location",Toast.LENGTH_SHORT).show()
+        }
+
+        btnText.setOnClickListener {
+            Toast.makeText(this,"Succes Send Text",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
